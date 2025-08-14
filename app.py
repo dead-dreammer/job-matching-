4317bb5d00985242d85b9845d3513ebd4e70cf56
Dalziel Sarkhot <dalziel.sarkhot94@gmail.com>
2025-08-14 18:46:46 +0200

    Connected fe to be

--- app.py
+++ app.py
@@ -3,12 +3,26 @@ from flask import Flask, render_template # Import necessary libraries
 # Create an instance of the Flask application
 app = Flask(__name__) # The __name__ variable tells Flask where to look for templates and static files
 
-# Create a route for the homapage
+# Create routes for all pages
 @app.route('/') 
-# Function will run if user accesses the above root
 def home():
-    # Renders the html file
-    return render_template('') # -- HTML FILE GOES HERE -- 
+    return render_template('Index.html')
+
+@app.route('/about')
+def about():
+    return render_template('AboutUs.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('Contact.html')
+
+@app.route('/login')
+def login():
+    return render_template('LoginPage.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('SignUp.html')
 
 # Server will only run if this file is executed directly
 # Prevents the server from running if the file is imported into another module
